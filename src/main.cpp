bd7bcd79acaf5016f24ed7ae372f1d4b4d0f497e
grillow <spmr757@gmail.com>
2021-07-03 20:50:14 +0300

    fill island function works

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,96 @@
 #include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
 
-int main() {
-    std::cout << "Hello World!" << std::endl;
-    return 0;
+
+void inputmap(std::istream & in, std::vector<std::vector<int8_t>> & map) {
+    for (auto & row : map) {
+        for (auto & e : row) {
+            int i = 0;
+            in >> i;
+            e = static_cast<int8_t>(i);
+        }
+    }
 }
 
+void outputmap(std::ostream & out, std::vector<std::vector<int8_t>> & map) {
+    for (auto & row : map) {
+        for (auto & e : row) {
+            //out << (e ? 1 : 0) << ' ';
+            out << static_cast<int16_t>(e) << ' ';
+        }
+        out << '\n';
+    }
+    out << std::flush;
+}
+
+
+struct pos {
+    int i;
+    int j;
+};
+
+
+constexpr inline bool isValid(const pos & point, const int borderX, const int borderY) {
+        return (point.i >= 0 && point.j >= 0 && point.i < borderX && point.j < borderY);
+}
+
+
+// I am forced to use int8_t instead of bool since stl decided to break std::vector<bool>
+size_t floodfill(std::vector<std::vector<int8_t>> map, const pos start) {
+    const size_t M = map.size();    if (M == 0) throw std::runtime_error("M should be not 0");
+    const size_t N = map[0].size(); if (N == 0) throw std::runtime_error("N should be not 0");
+    
+    size_t visited = 0;
+    if (!map[start.j][start.i]) {
+        return visited;
+    }
+    
+    std::stack<pos> frontier;
+    
+    frontier.emplace(start);
+    map[start.j][start.i] = false;
+    ++visited;
+    while (!frontier.empty()) {
+        const auto c = frontier.top();
+        frontier.pop();
+
+        for (const auto & next : {
+            pos { c.i    , c.j + 1 },
+            pos { c.i    , c.j - 1 },
+            pos { c.i + 1, c.j     },
+            pos { c.i - 1, c.j     }
+        }) {
+            if(!isValid(next, N, M)) continue;
+            if (map[next.j][next.i]) {
+                frontier.emplace(next);
+                map[next.j][next.i] = false;
+                ++visited;
+            }
+        }
+    }
+    std::cout << "removed that island:" << std::endl;
+    outputmap(std::cout, map);
+    return visited;
+}
+
+
+int main() {
+    size_t n = 0;
+    std::cin >> n;
+    size_t m = 0;
+    std::cin >> m;
+    
+    std::vector<std::vector<int8_t>> map(m, std::vector<int8_t>(n, false));
+    inputmap(std::cin, map);
+    outputmap(std::cout, map);
+    std::cout << floodfill(map, {2, 0}) << std::endl;
+    std::cout << floodfill(map, {7, 0}) << std::endl;
+    std::cout << floodfill(map, {1, 2}) << std::endl;
+    std::cout << floodfill(map, {4, 2}) << std::endl;
+    std::cout << floodfill(map, {8, 3}) << std::endl;
+    std::cout << floodfill(map, {7, 6}) << std::endl;
+
+    return 0;
+}
