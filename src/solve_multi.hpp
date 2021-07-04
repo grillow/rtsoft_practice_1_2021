ff955421260edf2aacfdc87f4a9fd770f8b340f9
grillow <spmr757@gmail.com>
2021-07-04 22:08:06 +0300

    thread->async + pipeline flex

--- src/solve_multi.hpp
+++ src/solve_multi.hpp
@@ -4,6 +4,7 @@
 #include "floodfill.hpp"
 
 #include <algorithm>
+#include <iterator>
 #include <ranges>
 #include <thread>
 #include <future>
@@ -41,37 +42,25 @@ size_t worker(const std::vector<std::vector<int8_t>> & map, const size_t begin, 
 
 
 // multi-threaded
-size_t solve_multi(std::vector<std::vector<int8_t>> map, const size_t CPU) {
+size_t solve_multi(const std::vector<std::vector<int8_t>> & map, const size_t CPU) {
     const size_t M = map.size();    if (M == 0) throw std::runtime_error("M should be greater than 0");
     const size_t N = map[0].size(); if (N == 0) throw std::runtime_error("N should be greater than 0");
         
     const size_t total = N * M;
     const size_t piece_size = total / CPU;
     
-    std::vector<std::future<size_t>> max_squares(CPU);
+    std::vector<std::future<size_t>> max_squares;
+    max_squares.reserve(CPU);
 
-    std::generate(max_squares.begin(), max_squares.end(),
-        [&, i = 0u](){
+    std::generate_n(std::back_inserter(max_squares), CPU,
+        [&, i = 0u]() mutable {
             const size_t begin = i * piece_size;
             const size_t end = (i + 1) * piece_size + ( (i == CPU - 1) * (total - piece_size * CPU) );   // tail goes to the last thread
+            ++i;
             return std::async(worker, std::ref(map), begin, end);
         }
     );
 
-    /*
-    std::vector<std::thread> workers; workers.reserve(CPU);
-    for (size_t i = 0; i < CPU; ++i) {
-        const size_t total = N * M;
-        const size_t piece_size = total / CPU;
-        const size_t begin = i * piece_size;
-        const size_t end = (i + 1) * piece_size + ( (i == CPU -1) * (total - piece_size * CPU) );   // tail goes to the last thread
-        workers.emplace_back(worker_f, std::ref(map), begin, end, std::ref(max_squares[i]));
-    }
-    for (auto & worker : workers) {
-        worker.join();
-    }
-    */
-
-    const size_t max_square = std::max(max_squares | std::ranges::views::transform( [](auto & future){ return future.get(); } ));
+    const size_t max_square = std::ranges::max(max_squares | std::ranges::views::transform( [](auto & future){ return future.get(); } ));
     return max_square;
 }
