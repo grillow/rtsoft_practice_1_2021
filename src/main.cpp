67d6e36b46860d36067eb7f1eb8553e448ebae0d
grillow <spmr757@gmail.com>
2021-07-04 17:14:04 +0300

    one-threaded version is done

--- src/main.cpp
+++ src/main.cpp
@@ -38,20 +38,20 @@ constexpr inline bool isValid(const point & pos, const int borderX, const int bo
 
 
 // I am forced to use int8_t instead of bool since stl decided to break std::vector<bool>
-std::vector<point> floodfill(std::vector<std::vector<int8_t>> map, const point start) {
+size_t floodfill(std::vector<std::vector<int8_t>> & map, const point start) {
     const size_t M = map.size();    if (M == 0) throw std::runtime_error("M should be greater than 0");
     const size_t N = map[0].size(); if (N == 0) throw std::runtime_error("N should be greater than 0");
 
-    std::vector<point> visited;
-    if (!map[start.X][start.Y]) {
+    size_t visited = 0;
+    if (!map[start.Y][start.X]) {
         return visited;
     }
     
     std::stack<point> frontier;
     
     frontier.emplace(start);
-    map[start.X][start.Y] = false;
-    visited.emplace_back(start);
+    map[start.Y][start.X] = false;
+    ++visited;
     while (!frontier.empty()) {
         const auto c = frontier.top();
         frontier.pop();
@@ -63,10 +63,10 @@ std::vector<point> floodfill(std::vector<std::vector<int8_t>> map, const point s
             point { c.X - 1, c.Y     }
         }) {
             if(!isValid(next, N, M)) continue;
-            if (map[next.X][next.Y]) {
+            if (map[next.Y][next.X]) {
                 frontier.emplace(next);
-                map[next.X][next.Y] = false;
-                visited.emplace_back(next);
+                map[next.Y][next.X] = false;
+                ++visited;
             }
         }
     }
@@ -80,9 +80,9 @@ int main() {
     size_t M = 0;
     std::cin >> M;
     
-    std::vector<std::vector<int8_t>> map(N, std::vector<int8_t>(M, false));
+    std::vector<std::vector<int8_t>> map(M, std::vector<int8_t>(N, false));
     inputmap(std::cin, map);
-    //outputmap(std::cout, map);
+    outputmap(std::cout, map);
     
     const size_t total = N * M;
     size_t max_square = 0;
@@ -91,12 +91,10 @@ int main() {
         const size_t X = current_position % N;
         const size_t Y = current_position / N;
         const auto island = floodfill(map, point { static_cast<int>(X), static_cast<int>(Y) });
-        max_square = std::max(max_square, island.size());
-        for (const auto & dot : island) {
-            map[dot.X][dot.Y] = false;
-        }
+        max_square = std::max(max_square, island);
         ++current_position;
     }
     std::cout << max_square << std::endl;
+    outputmap(std::cout, map);
     return 0;
 }
